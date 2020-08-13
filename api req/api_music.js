a0765d70d617e34a66520d3a4ae606feab1a99d4
Natallia Stelmakh <stnata.coding@gmail.com>
2020-08-13 14:06:28 -0400

    Added api request for jazz music

---
+++ api req/api_music.js
@@ -0,0 +1,21 @@
+/* Load the HTTP library */
+var unirest = require("unirest");
+
+var req = unirest("GET", "https://deezerdevs-deezer.p.rapidapi.com/search");
+
+req.query({
+    "q": "jazz"
+});
+
+req.headers({
+    "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
+    "x-rapidapi-key": "4eb5968d74msh7a28f9eece9c17ap13c40djsnde5aaab47e32",
+    "useQueryString": true
+});
+
+
+req.end(function (res) {
+    if (res.error) throw new Error(res.error);
+
+    console.log(res.body);
+});
