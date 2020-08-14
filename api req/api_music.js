ca671a90fb0b7f27de0984c26137acc79622c2fd
Natallia Stelmakh <stnata.coding@gmail.com>
2020-08-14 13:10:29 -0400

    Audio data is logged

--- api req/api_music.js
+++ api req/api_music.js
@@ -1,34 +1,41 @@
-/* Load the HTTP library */
-var unirest = require("unirest");
+
+const axios = require("axios");
 require("dotenv").config()
 
-// const music = {
-function getComposition(audioTitle, audioFile) {
 
-    var req = unirest("GET", "https://deezerdevs-deezer.p.rapidapi.com/search");
-
-    req.query({
-        "q": "jazz"
-    });
-
-    req.headers({
-        "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
-        "x-rapidapi-key": "4eb5968d74msh7a28f9eece9c17ap13c40djsnde5aaab47e32",
-        // process.env.PASSW_DW,
-        "useQueryString": true
-    });
+function getComposition() {
 
 
-    req.end(function (res) {
-        if (res.error) throw new Error(res.error);
-
-        console.log(res.body);
-        audioTitle = res.body.title_short;
-        audioFile = res.body.link;
-        console.log("audioFile:", audioFile)
-        console.log("audioTitle:", audioTitle)
+    axios({
+        "method": "GET",
+        "url": "https://deezerdevs-deezer.p.rapidapi.com/search",
+        "headers": {
+            "content-type": "application/octet-stream",
+            "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
+            "x-rapidapi-key": "4eb5968d74msh7a28f9eece9c17ap13c40djsnde5aaab47e32",
+            "useQueryString": true
+        }, "params": {
+            "q": "jazz"
+        }
     })
+        .then((response) => {
+
+            const songs = response.data.data;
+            console.log(songs)
+            songs.map(song => {
+                let audioTitle = song.title_short;
+                let audioFile = song.preview;
+                console.log("audioFile:", audioFile)
+                console.log("audioTitle:", audioTitle)
+            })
+
+
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+
 
 }
 
-getComposition()
+module.exports = getComposition
