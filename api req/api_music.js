e87b38bde3e0b111ad5ed5a882abd3ac9de92fb3
Natallia Stelmakh <stnata.coding@gmail.com>
2020-08-14 12:33:09 -0400

    audiofile

--- api req/api_music.js
+++ api req/api_music.js
@@ -2,22 +2,33 @@
 var unirest = require("unirest");
 require("dotenv").config()
 
+// const music = {
+function getComposition(audioTitle, audioFile) {
 
-var req = unirest("GET", "https://deezerdevs-deezer.p.rapidapi.com/search");
+    var req = unirest("GET", "https://deezerdevs-deezer.p.rapidapi.com/search");
 
-req.query({
-    "q": "jazz"
-});
+    req.query({
+        "q": "jazz"
+    });
 
-req.headers({
-    "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
-    "x-rapidapi-key": process.env.PASSW_DW,
-    "useQueryString": true
-});
+    req.headers({
+        "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
+        "x-rapidapi-key": "4eb5968d74msh7a28f9eece9c17ap13c40djsnde5aaab47e32",
+        // process.env.PASSW_DW,
+        "useQueryString": true
+    });
 
 
-req.end(function (res) {
-    if (res.error) throw new Error(res.error);
+    req.end(function (res) {
+        if (res.error) throw new Error(res.error);
 
-    console.log(res.body);
-});
+        console.log(res.body);
+        audioTitle = res.body.title_short;
+        audioFile = res.body.link;
+        console.log("audioFile:", audioFile)
+        console.log("audioTitle:", audioTitle)
+    })
+
+}
+
+getComposition()
