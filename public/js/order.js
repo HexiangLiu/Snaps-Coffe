f8ce8786371bd0190f2f9ac1b61cfac4d67beb2a
Henry Liu <hl3184@columbia.edu>
2020-08-14 20:32:40 -0400

    spoonacular api route and front end complete

--- public/js/order.js
+++ public/js/order.js
@@ -1,7 +1,6 @@
-const place_btn = $('.submit_order');
-const title = $('.title');
-const external_routes = '';
-var btn = $('.itemBtn');
+const detail_container = $('.item-detail');
+
+var btn = $('.menu-item');
 // const getComposition = require("./utils/api_music.js")
 $.ajax({
   url: '/api/music',
@@ -14,20 +13,41 @@ $.ajax({
     console.log('audioTitle:', audioTitle);
   });
 
-  btn.on('click', () => {
+  btn.on('click', (e) => {
     //make ajax request
+    let q;
 
+    if (e.target.matches('h3')) {
+      q = `q=${$(e.target).text()}`;
+    } else if (e.target.matches('li')) {
+      q = `q=${$(e.target.lastElementChild).text()}`;
+    } else if (e.target.matches('img')) {
+      q = `q=${$(e.target.nextElementSibling).text()}`;
+    }
     $.ajax({
-      url: '/api/spoonacular',
+      url: '/api/spoonacular/?' + q,
       method: 'GET',
     }).then(function (menuItems) {
-      menuItems.map((menuItem) => {
-        let itemTitle = menuItem.title;
-        let itemImage = menuItem.image;
-        let itemServing = menuItem.servingSize;
-        console.log(itemTitle);
-        console.log(itemImage);
-        console.log(itemServing);
+      detail_container.empty();
+
+      detail_container.append(`<div class="card">
+      <img src="${menuItems[0].image}"
+          class="card-img-top item-img" alt="${menuItems[0].title}">
+      <div class="card-body">
+          <h5 class="card-title title">${menuItems[0].title}</h5>
+          <p class="card-text">Serving Size: ${
+            menuItems[0].servingSize ? menuItems[0].servingSize : 1
+          }</p>
+          <button class="btn btn-warning btn-block text-white submit_order">Place order</button>
+      </div>
+  </div>`);
+
+      $('.submit_order').on('click', () => {
+        console.log('clicked');
+        $.post('/api/orders', { name: $('.title').text() }).then((order) => {
+          console.log(order);
+          location.reload();
+        });
       });
     });
   });
@@ -39,11 +59,3 @@ $.ajax({
 // //append it to the page
 // $("body").append(music)
 // });
-
-place_btn.on('click', () => {
-  console.log('clicked');
-  $.post('/api/orders', { name: title.text() }).then((order) => {
-    console.log(order);
-    location.reload();
-  });
-});
--- routes/external-api-routes.js
+++ routes/external-api-routes.js
@@ -1,18 +1,19 @@
-const { getComposition } = require("../utils/api_music.js")
-const { menuQuery } = require("../utils/spoonacular.js")
+const { getComposition } = require('../utils/api_music.js');
+const { menuQuery } = require('../utils/spoonacular.js');
 
 module.exports = function (app) {
-    app.get('/api/music', async function (req, res) {
-        const response = await getComposition()
-        // console.log("IAM WOKRING")
-        console.log(response)
-        res.json(response.data.data)
-    })
+  app.get('/api/music', async function (req, res) {
+    const response = await getComposition();
+    // console.log("IAM WOKRING")
+    // console.log(response);
+    res.json(response.data.data);
+  });
 
-    app.get('/api/spoonacular', async function (req, res) {
-        const response = await menuQuery("frappuccino");
-        // console.log("Werking?");
-        console.log(response);
-        res.json(response.data.menuItems);
-    })
-}
+  app.get('/api/spoonacular', async function (req, res) {
+    console.log(req.query.q);
+    const response = await menuQuery(req.query.q);
+    // console.log("Werking?");
+    // console.log(response);
+    res.json(response.data.menuItems);
+  });
+};
